b14765cea71ab8b2003c1476c9fc926d856cb2f3
femke <femke@192.168.0.115>
2018-12-31 14:40:20 +0100

    javadoc toegevoegd

--- src/spaceinvaders/Alien.java
+++ src/spaceinvaders/Alien.java
@@ -10,50 +10,69 @@ package spaceinvaders;
  * @author vantichelen
  */
 public class Alien {
+    //datamembers
     public static int x;
     public static int y;
-
+    
+    //constructor
     public Alien(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
     /**
-     * @return the x
+     * wat is het x-Coördinaat van de Alien
+     * @return het x-coördinaat
      */
     public int getX() {
         return x;
     }
 
     /**
-     * @param x the x to set
+     * verander het x-coördinaat
+     * @param x het nieuwe x-coördinaat
      */
     public void setX(int x) {
         this.x = x;
     }
 
     /**
-     * @return the y
+     * wat is het y-coördinaat van de alien?
+     * @return het y-coördinaat
      */
     public int getY() {
         return y;
     }
 
     /**
-     * @param y the y to set
+     * verander het y-coördinaat van de Alien
+     * @param y nieuwe y-coördinaat
      */
     public void setY(int y) {
         this.y = y;
     }
+    /**
+     * beweeg naar rechts
+     */
     public void BeweegRechts(){
         x++;
     }
+    /** 
+     * beweeg naar links
+     */
     public void BeweegLinks(){
         x--;
     }
+    /**
+     * beweeg omlaag
+     */
     public void BeweegOmlaag(){
         y++;
     }
+    /**
+     * is de alien geraakt door een kogel
+     * @return true of false
+     */
     public boolean IsGeraakt(){
         if (Kogel.x == this.x && Kogel.y == this.y){
             return true;
