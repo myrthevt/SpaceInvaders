ba451f05564f54f3cc6fba67a2519a655e83b322
femke <femke@192.168.0.115>
2018-12-31 14:44:24 +0100

    javadoc toegevoegd

--- src/spaceinvaders/SpaceInvaders.java
+++ src/spaceinvaders/SpaceInvaders.java
@@ -9,11 +9,13 @@ package spaceinvaders;
  *
  * @author vantichelen
  */
+
 public class SpaceInvaders {
+    //datamembers
     private int breedte;
     private int hoogte;
     private int punten;
-
+    //constructor
     public SpaceInvaders(int breedte, int hoogte, int punten) {
         this.breedte = breedte;
         this.hoogte = hoogte;
@@ -21,42 +23,47 @@ public class SpaceInvaders {
     }
 
     /**
-     * @return the breedte
+     * wat is de breedte van het veld waar het spel in wordt gespeeld?
+     * @return de breedte
      */
     public int getBreedte() {
         return breedte;
     }
 
     /**
-     * @param breedte the breedte to set
+     * verander de breedte 
+     * @param breedte de nieuwe breedte
      */
     public void setBreedte(int breedte) {
         this.breedte = breedte;
     }
 
     /**
-     * @return the hoogte
+     * wat is de hoogte van het veld waar het spel in wordt gespeeld?
+     * @return de hoogte
      */
     public int getHoogte() {
         return hoogte;
     }
 
     /**
-     * @param hoogte the hoogte to set
+     * verander de hoogte
+     * @param hoogte de nieuwe hoogte
      */
     public void setHoogte(int hoogte) {
         this.hoogte = hoogte;
     }
 
     /**
-     * @return the punten
+     * wat is het aantal behaalde punten
+     * @return de punten
      */
     public int getPunten() {
         return punten;
     }
 
-    /**
-     * @param punten the punten to set
+    /** verander de punten
+     * @param punten de nieuwe punten
      */
     public void setPunten(int punten) {
         this.punten = punten;
