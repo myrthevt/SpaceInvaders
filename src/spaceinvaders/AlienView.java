8cf8fcb40b1956d71afefd2de00a66a97d44fdd6
femke <femke@192.168.0.115>
2019-01-02 13:37:03 +0100

    ImageView toegevoegd aan AlienView

--- src/spaceinvaders/AlienView.java
+++ src/spaceinvaders/AlienView.java
@@ -5,10 +5,26 @@
  */
 package spaceinvaders;
 
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Region;
+
 /**
  *
  * @author vantichelen
  */
-public class AlienView {
+public class AlienView extends Region {
+    private Alien model;
+
+    public AlienView(Alien model) {
+        this.model = model;
+        update();
+    }
+    public void update(){
+        ImageView alien = new ImageView("49742769_2163177273899539_4121474896691724288_n");
+        getChildren().add(alien);
+    }
+    
+    
     
 }
+
