bd186ebca5e0a63d64fc425ad824683023294821
femke <femke@192.168.0.115>
2019-01-10 15:55:59 +0100

    initialize methode aangepast methode beweegKanon gemaakt andere methodes in commentaar

--- src/spaceinvaders/FXMLSpaceInvadersViewController.java
+++ src/spaceinvaders/FXMLSpaceInvadersViewController.java
@@ -10,7 +10,6 @@ import java.awt.event.KeyEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -27,28 +26,33 @@ public class FXMLSpaceInvadersViewController {
     @FXML
     private AnchorPane achtergrond;
 
-    @FXML
-    private ImageView achtergrondSpel;
-
-    @FXML
-    private ImageView alien;
-
-
+    
     private SpaceInvadersModel model; // controller moet eigenschappen van het model kunnen veranderen
     private SpaceInvadersView view; 
     
     @FXML
     void initialize() {
-        assert achtergrond != null : "fx:id=\"achtergrond\" was not injected: check your FXML file 'FXMLSpaceInvadersView.fxml'.";
-
+        achtergrond.setOnKeyPressed(this::beweegKanon);
     }
+    
     public void setModel(SpaceInvadersModel model){
         this.model = model;
         view = new SpaceInvadersView (model);
         achtergrond.getChildren().add(view);
     }
+     public void beweegKanon (javafx.scene.input.KeyEvent e){
+        switch(e.getCode()){
+                case RIGHT:
+                    LaserKanon.beweegNaarRechts();
+                //case KP_UP:
+                //case SPACE:
+                //case Z:    
+                    //model.boven();
+                    //update();
+                    //break;
+    }
     
-    public void beweegNaarRechts(KeyEvent event){
+    //public void beweegNaarRechts(KeyEvent event){
         
         
         
