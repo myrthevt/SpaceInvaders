6085f09245afeb7ae7dda4ad7ffff094020dd217
femke <femke@192.168.0.115>
2019-01-10 12:02:11 +0100

    update methode gemaakt in de spaceinvadersview

--- src/spaceinvaders/SpaceInvadersView.java
+++ src/spaceinvaders/SpaceInvadersView.java
@@ -5,11 +5,35 @@
  */
 package spaceinvaders;
 
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
 /**
  *
  * @author femke
  */
-public class SpaceInvadersView {
+public class SpaceInvadersView extends Region {
+    private SpaceInvaders model;
+    
+    public SpaceInvadersView (SpaceInvaders model){
+        this.model = model;
+        update ();
+    }
+    
+ 
     
     
-}
+           
+    public void update(){
+      getChildren().clear();
+      Circle kogel = new Circle(Kogel.getX(), Kogel.getY(), 5, Color.VIOLET);
+      getChildren().add(kogel);
+      Rectangle steen = new Rectangle(295,1, 70, 200);
+      getChildren().add(steen);
+      
+      }
+     
+          
+    }
