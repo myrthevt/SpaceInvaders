1a6f01bc2473e86643fa6fedea4ca1ba72cd604d
femke <femke@192.168.0.115>
2019-01-12 13:23:29 +0100

    nieuwe klasse KogelBeweger voor TimerTask toegevoeg, doet nog niks

---
+++ src/spaceinvaders/KogelBeweger.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spaceinvaders;
+
+import java.util.TimerTask;
+import javafx.application.Platform;
+
+/**
+ *
+ * @author femke
+ */
+public class KogelBeweger extends TimerTask {
+    private SpaceInvadersModel model;
+    private SpaceInvadersView view;
+    
+    
+    public KogelBeweger (SpaceInvadersModel model, SpaceInvadersView view){
+        this.model = model;
+        this.view = view;
+    }
+    
+
+    @Override
+    public void run() {
+        model.getKogel().BeweegNaarBoven();
+        Platform.runLater(()->view.update());
+    }
+    
+    
+}
